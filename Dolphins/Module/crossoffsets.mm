575d62cb038a3aef6094b4eeea4b65e7a8e51544
oc9201744-cmd <oc9201744@gmail.com>
2026-03-14 01:14:00 +0300

    Update crossoffsets.mm

--- Dolphins/Module/crossoffsets.mm
+++ Dolphins/Module/crossoffsets.mm
@@ -3,17 +3,23 @@
 @implementation OffsetsManager
 
 + (OffsetValues)getOffsetsForBundleID:(NSString *)bundleID {
-    OffsetValues defaultOffsets = { 0x102a62208, 0x10a566e00, 0x104bd8740, 0x10a1178b0 };
+    // Default offsets (updated)
+    OffsetValues defaultOffsets = {
+        0x104bd8740,  // GName Fun
+        0x10a1178b0,  // GName Data
+        0x102a62208,  // GWorld Fun
+        0x10a566e00   // GWorld Data
+    };
 
-if ([bundleID containsString:@"tencent"]) {
-    return (OffsetValues){ 0x102a62208, 0x10a566e00, 0x104bd8740, 0x10a1178b0 };
-} else if ([bundleID containsString:@"vng"]) {
-    return (OffsetValues){ 0x1028791CC, 0x10A171A00, 0x104510EF0, 0x109AAA1A0 };
-} else if ([bundleID containsString:@"krmobile"]) {
-    return (OffsetValues){ 0x102AD71F8, 0x10A47D400, 0x10476F14C, 0x109DB5940 };
-} else if ([bundleID containsString:@"rekoo"]) {
-    return (OffsetValues){ 0x102AAAB0C, 0x10A453300, 0x104742830, 0x109D8B830 };
-}
+    if ([bundleID containsString:@"tencent"]) {
+        return (OffsetValues){ 0x102A5125C, 0x10A4A1960, 0x104C0F1E8, 0x10A0557E0 };
+    } else if ([bundleID containsString:@"vng"]) {
+        return (OffsetValues){ 0x1028791CC, 0x10A171A00, 0x104510EF0, 0x109AAA1A0 };
+    } else if ([bundleID containsString:@"krmobile"]) {
+        return (OffsetValues){ 0x102AD71F8, 0x10A47D400, 0x10476F14C, 0x109DB5940 };
+    } else if ([bundleID containsString:@"rekoo"]) {
+        return (OffsetValues){ 0x102AAAB0C, 0x10A453300, 0x104742830, 0x109D8B830 };
+    }
 
     return defaultOffsets;
 }
