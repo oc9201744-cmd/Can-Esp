9eca23437e163fdd80eebe90d9ec4af092c67b08
oc9201744-cmd <oc9201744@gmail.com>
2026-03-13 23:53:28 +0300

    Update default offsets for bundle IDs

--- Dolphins/Module/crossoffsets.mm
+++ Dolphins/Module/crossoffsets.mm
@@ -3,10 +3,10 @@
 @implementation OffsetsManager
 
 + (OffsetValues)getOffsetsForBundleID:(NSString *)bundleID {
-    OffsetValues defaultOffsets =  { 0x104C0F1E8, 0x10A0557E0, 0x1045AA3E8, 0x109BB4440 };  // Ваши значения по умолчанию
+    OffsetValues defaultOffsets = { 0x102a62208, 0x10a566e00, 0x104bd8740, 0x10a1178b0 };
 
 if ([bundleID containsString:@"tencent"]) {
-    return (OffsetValues){ 0x102A5125C, 0x10A4A1960, 0x104C0F1E8, 0x10A0557E0 };
+    return (OffsetValues){ 0x102a62208, 0x10a566e00, 0x104bd8740, 0x10a1178b0 };
 } else if ([bundleID containsString:@"vng"]) {
     return (OffsetValues){ 0x1028791CC, 0x10A171A00, 0x104510EF0, 0x109AAA1A0 };
 } else if ([bundleID containsString:@"krmobile"]) {
