9d4ebc838c5513787ac60111934c621f35ad1b9f
oc9201744-cmd <oc9201744@gmail.com>
2026-03-15 01:07:53 +0300

    Fix offset values and player controller pointer retrieval

--- Dolphins/Dolphins.mm
+++ Dolphins/Dolphins.mm
@@ -57,7 +57,7 @@ MemoryTools memoryTools;
 //OffsetSet currentOffsetSet = GL;
 
 OffsetValues offsets[] = {
-    { 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10A34E980 },  // GL
+    { 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0 },  // GL
     { 0x1028791CC, 0x10A171A00, 0x104510EF0, 0x109AAA1A0 },  // VNG
     { 0x102AD71F8, 0x10A47D400, 0x10476F14C, 0x109DB5940 },  // KR
     { 0x102AAAB0C, 0x10A453300, 0x104742830, 0x109D8B830 }   // TW
@@ -169,7 +169,7 @@ void *readStaticData(void *) {
             staticData.gwlordAddr = gWorld();
             staticData.gnameAddr = gName();
             //角色控制器
-            staticData.playerController = memoryTools.readPtr(memoryTools.readPtr(memoryTools.readPtr(memoryTools.readPtr(staticData.gwlordAddr + PubgOffset::PlayerControllerOffset[0]) + PubgOffset::PlayerControllerOffset[1]) + 0x0) + PubgOffset::PlayerControllerOffset[2]);
+            staticData.playerController = memoryTools.readPtr(memoryTools.readPtr(memoryTools.readPtr(staticData.gwlordAddr + PubgOffset::PlayerControllerOffset[0]) + PubgOffset::PlayerControllerOffset[1]) + PubgOffset::PlayerControllerOffset[2]);
             //掩体判断
             LineOfSightTo = (bool (*)(void *, void *, ImVec3, bool)) (memoryTools.readPtr(memoryTools.readPtr(staticData.playerController + 0x0) + PubgOffset::PlayerControllerParam::ControllerFunction::LineOfSightToOffset));//0x780
             //自己指针
