fe59be55dde8ccec3a50bd5681daba5130cc6370
oc9201744-cmd <oc9201744@gmail.com>
2026-03-15 01:08:41 +0300

    Fix default offsets for GL in crossoffsets.mm

--- Dolphins/Module/crossoffsets.mm
+++ Dolphins/Module/crossoffsets.mm
@@ -3,10 +3,10 @@
 @implementation OffsetsManager
 
 + (OffsetValues)getOffsetsForBundleID:(NSString *)bundleID {
-    OffsetValues defaultOffsets =  { 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10A34E980 };  // GL
+    OffsetValues defaultOffsets =  { 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0 };  // GL
 
 if ([bundleID containsString:@"tencent"]) {
-    return (OffsetValues){ 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10A34E980 };
+    return (OffsetValues){ 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0 };
 } else if ([bundleID containsString:@"vng"]) {
     return (OffsetValues){ 0x1028791CC, 0x10A171A00, 0x104510EF0, 0x109AAA1A0 };
 } else if ([bundleID containsString:@"krmobile"]) {
