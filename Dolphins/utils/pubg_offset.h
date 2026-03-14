c2b8cb9a3838dff84557b2a793ce0283ffe51aa4
oc9201744-cmd <oc9201744@gmail.com>
2026-03-15 00:20:36 +0300

    Update pubg_offset.h

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -3,7 +3,7 @@
 
 namespace PubgOffset {
 
-int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
+int PlayerControllerOffset[3] = {0x470, 0x48, 0x30};
 
 namespace PlayerControllerParam {
 
@@ -25,8 +25,8 @@ int LineOfSightToOffset = 0x7B0;
 int ULevelOffset = 0x30;
 
 namespace ULevelParam {
-int ObjectArrayOffset = 0xA0;
-int ObjectCountOffset = 0xA8;
+int ObjectArrayOffset = 0x28;
+int ObjectCountOffset = 0x30;
 }
 
 namespace ObjectParam {
