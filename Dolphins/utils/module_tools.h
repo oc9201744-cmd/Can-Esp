f62acb0260a4654fddd4cb4043f16464b6dc650f
oc9201744-cmd <oc9201744@gmail.com>
2026-03-16 00:41:50 +0300

    Update module_tools.h

--- Dolphins/utils/module_tools.h
+++ Dolphins/utils/module_tools.h
@@ -62,10 +62,8 @@ struct MainSwitch {
     bool playerStatus;
     bool materialStatus;
     bool aimbotStatus;
-         bool gzb;;
-        
-
-    
+    bool botStatus;     // Bot ESP göster/gizle (true = bot göster)
+    bool gzb;
 };
 
 
@@ -279,5 +277,3 @@ MaterialStruct isMaterial(const char *name);
 //是否为物资(盒子)
 MaterialStruct isBoxMaterial(int box_goods_id);
 #endif /* CustomStrcut_h */
-
-
