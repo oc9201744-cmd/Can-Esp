2636f990564e37e503fd35e762f3fd1251a0ac2a
oc9201744-cmd <oc9201744@gmail.com>
2026-03-15 19:33:45 +0300

    Update crossoffsets.mm

--- Dolphins/Module/crossoffsets.mm
+++ Dolphins/Module/crossoffsets.mm
@@ -3,10 +3,10 @@
 @implementation OffsetsManager
 
 + (OffsetValues)getOffsetsForBundleID:(NSString *)bundleID {
-    OffsetValues defaultOffsets =  { 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0 };  // GL
+    OffsetValues defaultOffsets = { 0x102a62208, 0x10a566e00, 0x104bd8740, 0x10a1178b0 };
 
 if ([bundleID containsString:@"tencent"]) {
-    return (OffsetValues){ 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0 };
+    return (OffsetValues){ 0x102a62208, 0x10a566e00, 0x104bd8740, 0x10a1178b0 };
 } else if ([bundleID containsString:@"vng"]) {
     return (OffsetValues){ 0x1028791CC, 0x10A171A00, 0x104510EF0, 0x109AAA1A0 };
 } else if ([bundleID containsString:@"krmobile"]) {
