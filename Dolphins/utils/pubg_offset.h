c315b035c5cc47a9915584326e3471201e702826
oc9201744-cmd <oc9201744@gmail.com>
2026-03-15 05:14:20 +0300

    Update pubg_offset.h

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -3,17 +3,17 @@
 
 namespace PubgOffset {
 
-int PlayerControllerOffset[3] = {0x470, 0x48, 0x30};
+int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
 
 namespace PlayerControllerParam {
 
-int SelfOffset = 0x28d0;
+int SelfOffset = 0x28e0;
 int MouseOffset = 0x4e0;
 int CameraManagerOffset = 0x548;
 int AngleOffset = 0x558;
 
 namespace CameraManagerParam {
-int PovOffset = 0x520 + 0x10;
+int PovOffset = 0x10a0 + 0x10;
 }
 
 namespace ControllerFunction {
@@ -25,8 +25,8 @@ int LineOfSightToOffset = 0x7B0;
 int ULevelOffset = 0x30;
 
 namespace ULevelParam {
-int ObjectArrayOffset = 0x28;
-int ObjectCountOffset = 0x30;
+int ObjectArrayOffset = 0xA0;
+int ObjectCountOffset = 0xA8;
 }
 
 namespace ObjectParam {
@@ -41,7 +41,7 @@ int AddControllerPitchInputOffset = 0x898;
 }
 
 int StatusOffset = 0x1018;
-int TeamOffset = 0x1150;
+int TeamOffset = 0x998;
 int NameOffset = 0x15C8;
 int RobotOffset = 0x4968;
 int HpOffset = 0xe60;
@@ -54,23 +54,23 @@ int VehicleHPMaxOffset = 0x340;
 int VehicleFuelOffset = 0x424;
 int VehicleFuelMaxOffset = 0x420;
 
-int MoveCoordOffset = 0x518;
+int MoveCoordOffset = 0x110;
 int MeshOffset = 0x510;
 int boneCountOffset = 0x8d0;
 
 namespace MeshParam {
-int HumanOffset = 0xc40;
+int HumanOffset = 0x210;
 int BonesOffset = 0x988;
 }
 
 int OpenFireOffset = 0x1788;
-int OpenTheSightOffset = 0x10e1;
+int OpenTheSightOffset = 0x1134;
 
-int WeaponOneOffset = 0x25B8;
+int WeaponOneOffset = 0x2a30 + 0x20;
 
 namespace WeaponParam {
 
-int MasterOffset = 0x5C8;
+int MasterOffset = 0x110;
 int ShootModeOffset = 0x1089;
 int WeaponAttrOffset = 0x12c0;
 
@@ -81,17 +81,17 @@ int RecoilOffset = 0xcf0;
 
 }
 
-int GoodsListOffset = 0x2EF8;
+int GoodsListOffset = 0x940;
 
 namespace GoodsListParam {
-int DataBase = 0x490;
+int DataBase = 0x38;
 }
 
 int CoordOffset = 0x208;
 
 namespace CoordParam {
-int HeightOffset = 0x1ec;
-int CoordOffset = 0x1e4;
+int HeightOffset = 0x1dc;
+int CoordOffset = 0x1c8;
 }
 
 }
