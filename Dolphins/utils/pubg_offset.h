c98d50748097edce94cf0bce84293fe169763bdd
oc9201744-cmd <oc9201744@gmail.com>
2026-03-15 19:34:31 +0300

    Update pubg_offset.h

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -7,7 +7,7 @@ int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
 
 namespace PlayerControllerParam {
 
-int SelfOffset = 0x28d0;
+int SelfOffset = 0x28D0;
 int MouseOffset = 0x4e0;
 int CameraManagerOffset = 0x548;
 int AngleOffset = 0x558;
@@ -35,24 +35,24 @@ int ClassIdOffset = 0x18;
 int ClassNameOffset = 0xC;
 
 namespace PlayerFunction {
-int AddControllerYawInputOffset = 0x890;
-int AddControllerRollInputOffset = 0x888;
-int AddControllerPitchInputOffset = 0x898;
+int AddControllerYawInputOffset   = 0x890;
+int AddControllerRollInputOffset  = 0x898;
+int AddControllerPitchInputOffset = 0x888;
 }
 
 int StatusOffset = 0x1018;
 int TeamOffset = 0x998;
-int NameOffset = 0x4d0;
-int RobotOffset = 0x650;
-int HpOffset = 0xe60;
-int HpmaxOffset = 0xe64;
-int DeadOffset = 0xe7c;
+int NameOffset = 0x960;
+int RobotOffset = 0xA40;
+int HpOffset = 0xE60;
+int HpmaxOffset = 0xE64;
+int DeadOffset = 0xE7C;
 
-int VehicleCommonComponentOffset = 0xbf0;
-int VehicleHPOffset = 0x344;
-int VehicleHPMaxOffset = 0x340;
-int VehicleFuelOffset = 0x424;
-int VehicleFuelMaxOffset = 0x420;
+int VehicleCommonComponentOffset = 0xC00;
+int VehicleHPOffset = 0x354;
+int VehicleHPMaxOffset = 0x350;
+int VehicleFuelOffset = 0x43C;
+int VehicleFuelMaxOffset = 0x438;
 
 int MoveCoordOffset = 0x110;
 int MeshOffset = 0x510;
@@ -60,19 +60,23 @@ int boneCountOffset = 0x8d0;
 
 namespace MeshParam {
 int HumanOffset = 0x210;
-int BonesOffset = 0x988;
+int BonesOffset = 0xC40;
 }
 
-int OpenFireOffset = 0x1788;
-int OpenTheSightOffset = 0x1134;
+int OpenFireOffset = 0x1800;
+int OpenTheSightOffset = 0x3D48;
 
-int WeaponOneOffset = 0xa10;
+// 4.3: silah 2 adimda okunuyor
+// adim 1: character + WeaponManagerComponentOffset -> UCharacterWeaponManagerComponent*
+// adim 2: weaponMgr  + WeaponOneOffset             -> ASTExtraWeapon* (CurrentWeaponSimulate)
+int WeaponManagerComponentOffset = 0x25B8;
+int WeaponOneOffset = 0x05D8;
 
 namespace WeaponParam {
 
 int MasterOffset = 0x110;
-int ShootModeOffset = 0x1089;
-int WeaponAttrOffset = 0x12c0;
+int ShootModeOffset = 0x10D9;
+int WeaponAttrOffset = 0x1360;
 
 namespace WeaponAttrParam {
 int BulletSpeedOffset = 0x560;
@@ -81,17 +85,17 @@ int RecoilOffset = 0xcf0;
 
 }
 
-int GoodsListOffset = 0x2EF8;
+int GoodsListOffset = 0x940;
 
 namespace GoodsListParam {
-int DataBase = 0x490;
+int DataBase = 0x38;
 }
 
 int CoordOffset = 0x208;
 
 namespace CoordParam {
-int HeightOffset = 0x1dc;
-int CoordOffset = 0x1c8;
+int HeightOffset = 0x1EC;
+int CoordOffset = 0x1E4;
 }
 
 }
