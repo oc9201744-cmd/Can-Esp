0a3382eaf1be3cbbbb781f6319055ed91fc0224f
oc9201744-cmd <oc9201744@gmail.com>
2026-03-18 01:14:28 +0300

    Update Dolphins.mm

--- Dolphins/Dolphins.mm
+++ Dolphins/Dolphins.mm
@@ -242,16 +242,25 @@ void *readStaticData(void *) {
                     tmpPlayerData.team = team;
                     //名字
                     tmpPlayerData.name = getPlayerName(memoryTools.readPtr(objectAddr + PubgOffset::ObjectParam::NameOffset));
-                    // Bot tespiti - AIOHeader'dan doğrulandı
-                    // 1. AUAEPlayerState::MLAIDisplayUID (0x6D0) - replicate ediliyor, en güvenilir
+                    // Bot tespiti
                     bool isBot = false;
-                    uintptr_t ps = memoryTools.readPtr(objectAddr + 0x4D0);
-                    if (ps != 0) {
-                        uint64_t mlaiUID = 0;
-                        memoryTools.readMemory(ps + 0x6D0, 8, &mlaiUID);
-                        if (mlaiUID != 0) isBot = true;
+                    // 1. ASTExtraBaseCharacter::STExtraPlayerState (0x23B0) -> IsMLAI (0x10F8)
+                    uintptr_t stPS = memoryTools.readPtr(objectAddr + 0x23B0);
+                    if (stPS > 0x100000000 && stPS < 0x2000000000) {
+                        bool stIsMLAI = false;
+                        memoryTools.readMemory(stPS + 0x10F8, 1, &stIsMLAI);
+                        if (stIsMLAI) isBot = true;
                     }
-                    // 2. AUAECharacter::bIsAI (0xA40) + bIsMLAI (0xA41)
+                    // 2. APawn::PlayerState (0x4D0) -> AUAEPlayerState::MLAIDisplayUID (0x6D0)
+                    if (!isBot) {
+                        uintptr_t ps = memoryTools.readPtr(objectAddr + 0x4D0);
+                        if (ps > 0x100000000 && ps < 0x2000000000) {
+                            uint64_t mlaiUID = 0;
+                            memoryTools.readMemory(ps + 0x6D0, 8, &mlaiUID);
+                            if (mlaiUID != 0) isBot = true;
+                        }
+                    }
+                    // 3. AUAECharacter::bIsAI (0xA40) + bIsMLAI (0xA41)
                     if (!isBot) {
                         bool bIsAI = false, bIsMLAI = false;
                         memoryTools.readMemory(objectAddr + 0xA40, 1, &bIsAI);
