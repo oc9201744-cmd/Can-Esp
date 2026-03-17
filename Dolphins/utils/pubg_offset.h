f07afccca1425210d18c1a38dfddd8f322835617
oc9201744-cmd <oc9201744@gmail.com>
2026-03-17 05:55:13 +0300

    Refactor offsets in pubg_offset.h
    Updated various offsets for player and vehicle parameters in pubg_offset.h.

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -7,13 +7,13 @@ int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
 
 namespace PlayerControllerParam {
 
-int SelfOffset = 0x28D0;           // 4.3 SDK: STExtraBaseCharacter ptr
+int SelfOffset = 0x28e0;
 int MouseOffset = 0x4e0;
 int CameraManagerOffset = 0x548;
 int AngleOffset = 0x558;
 
 namespace CameraManagerParam {
-int PovOffset = 0x520 + 0x10;     // 4.3 SDK: CameraCache::POV
+int PovOffset = 0x10a0 + 0x10;
 }
 
 namespace ControllerFunction {
@@ -35,23 +35,25 @@ int ClassIdOffset = 0x18;
 int ClassNameOffset = 0xC;
 
 namespace PlayerFunction {
-int AddControllerYawInputOffset   = 0x890;
-int AddControllerRollInputOffset  = 0x888;
+int AddControllerYawInputOffset = 0x890;
+int AddControllerRollInputOffset = 0x888;
 int AddControllerPitchInputOffset = 0x898;
 }
 
-int StatusOffset = 0x1058;         // 4.3 SDK: CurrentStates
-int TeamOffset = 0x998;            // 4.3 SDK: TeamID
-int NameOffset = 0x960;            // 4.3 SDK: PlayerName
-int RobotOffset = 0xA40;           // 4.3 SDK: bIsAI (AUAECharacter)
-int HpOffset = 0xE60;              // 4.3 SDK: Health
-int HpmaxOffset = 0xE64;           // 4.3 SDK: HealthMax
-int DeadOffset = 0xE7C;            // 4.3 SDK: bDead
+int StatusOffset = 0x1018;
+int TeamOffset = 0x998;
+int NameOffset = 0x960;
+int RobotOffset = 0xa40;
+int MLRobotOffset = 0xa41;
+int HpOffset = 0xe60;
+int HpmaxOffset = 0xe64;
+int DeadOffset = 0xe7c;
+int PlayerStateOffset = 0x2308;
 
-int VehicleCommonComponentOffset = 0xC00;  // 4.3 SDK
+int VehicleCommonComponentOffset = 0xc00;
 int VehicleHPOffset = 0x354;
 int VehicleHPMaxOffset = 0x350;
-int VehicleFuelOffset = 0x43C;
+int VehicleFuelOffset = 0x43c;
 int VehicleFuelMaxOffset = 0x438;
 
 int MoveCoordOffset = 0x110;
@@ -60,23 +62,20 @@ int boneCountOffset = 0x8d0;
 
 namespace MeshParam {
 int HumanOffset = 0x210;
-int BonesOffset = 0xC40;           // 4.3 SDK: CachedComponentSpaceTransforms
+int BonesOffset = 0x988;
 }
 
-int OpenFireOffset = 0x1800;       // 4.3 SDK: bIsWeaponFiring
-int OpenTheSightOffset = 0x3D48;   // 4.3 SDK: bIsOpenWeaponSight
+int OpenFireOffset = 0x1800;
+int OpenTheSightOffset = 0x10e1;
+int WeaponManagerOffset = 0x25b8;
 
-// 4.3: 2 adimli okuma
-// Adim 1: character + WeaponManagerComponentOffset -> UCharacterWeaponManagerComponent*
-// Adim 2: weaponMgr + WeaponOneOffset -> ASTExtraWeapon* CurrentWeaponSimulate
-int WeaponManagerComponentOffset = 0x25B8;  // 4.3 SDK
-int WeaponOneOffset = 0x05D8;               // 4.3 SDK: CurrentWeaponSimulate
+int WeaponOneOffset = 0x2a30 + 0x20;
 
 namespace WeaponParam {
 
 int MasterOffset = 0x110;
-int ShootModeOffset = 0x10D9;      // 4.3 SDK
-int WeaponAttrOffset = 0x1360;     // 4.3 SDK: ShootWeaponEntityComp
+int ShootModeOffset = 0x10d9;
+int WeaponAttrOffset = 0x12c0;
 
 namespace WeaponAttrParam {
 int BulletSpeedOffset = 0x560;
