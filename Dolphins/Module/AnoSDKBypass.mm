8ff1d7252a2f8ca0c1b0972439948a35a73ba7b4
oc9201744-cmd <oc9201744@gmail.com>
2026-03-18 19:46:46 +0300

    Update AnoSDKBypass.mm

--- Dolphins/Module/AnoSDKBypass.mm
+++ Dolphins/Module/AnoSDKBypass.mm
@@ -24,14 +24,14 @@ static int h_InitEx(AnoSDKInitInfo *info, int flags) {
     if (info) info->tss_sdk_send_data_to_svr = (void *)fakeSend;
     return orig_InitEx ? orig_InitEx(info, flags) : 0;
 }
-static int   h_SUI(const char *a,int b,int c,int d,int e,int f,int g)                  { return 0; }
-static int   h_SUIL(const char *a,int b,int c,int d,int e,int f,int g,const char *h)   { return 0; }
-static int   h_Ioctl(int a, const void *b, int c)                                      { return 0; }
-static int   h_IoctlOld(int a, const void *b, int c)                                   { return 0; }
-static int   h_Pause(void)                                                             { return 0; }
-static int   h_Resume(void)                                                            { return 0; }
-static int   h_RecvData(const void *a, int b)                                          { return 0; }
-static int   h_RecvSig(const void *a, int b)                                           { return 0; }
+static int   h_SUI(const char *a,int b,int c,int d,int e,int f,int g)                 { return 0; }
+static int   h_SUIL(const char *a,int b,int c,int d,int e,int f,int g,const char *h)  { return 0; }
+static int   h_Ioctl(int a, const void *b, int c)    { return 0; }
+static int   h_IoctlOld(int a, const void *b, int c) { return 0; }
+static int   h_Pause(void)                           { return 0; }
+static int   h_Resume(void)                          { return 0; }
+static int   h_RecvData(const void *a, int b)        { return 0; }
+static int   h_RecvSig(const void *a, int b)         { return 0; }
 static void *h_GRD(int *l)  { if (l) *l = 0; return nullptr; }
 static void *h_GRD2(int *l) { if (l) *l = 0; return nullptr; }
 static void *h_GRD3(int *l) { if (l) *l = 0; return nullptr; }
@@ -47,25 +47,25 @@ void AnoSDKBypassInstall(void) {
     dispatch_once(&once, ^{
         static void *_1,*_2,*_3,*_4,*_5,*_6,*_7,*_8,*_9,*_10,*_11,*_12,*_13;
         struct rebinding b[] = {
-            {"AnoSDKInit",                   (void*)h_Init,    (void**)&orig_Init  },
-            {"AnoSDKInitEx",                 (void*)h_InitEx,  (void**)&orig_InitEx},
-            {"AnoSDKSetUserInfo",            (void*)h_SUI,     &_1 },
-            {"AnoSDKSetUserInfoWithLicense", (void*)h_SUIL,    &_2 },
-            {"AnoSDKIoctl",                  (void*)h_Ioctl,   &_3 },
-            {"AnoSDKIoctlOld",               (void*)h_IoctlOld,&_4 },
-            {"AnoSDKOnPause",                (void*)h_Pause,   &_5 },
-            {"AnoSDKOnResume",               (void*)h_Resume,  &_6 },
-            {"AnoSDKOnRecvData",             (void*)h_RecvData,&_7 },
-            {"AnoSDKOnRecvSignature",        (void*)h_RecvSig, &_8 },
-            {"AnoSDKGetReportData",          (void*)h_GRD,     &_9 },
-            {"AnoSDKGetReportData2",         (void*)h_GRD2,    &_10},
-            {"AnoSDKGetReportData3",         (void*)h_GRD3,    &_11},
-            {"AnoSDKGetReportData4",         (void*)h_GRD4,    &_12},
-            {"AnoSDKDelReportData",          (void*)h_Del,     (void**)&orig_Del  },
-            {"AnoSDKDelReportData3",         (void*)h_Del3,    (void**)&orig_Del3 },
-            {"AnoSDKDelReportData4",         (void*)h_Del4,    (void**)&orig_Del4 },
-            {"AnoSDKFree",                   (void*)h_Free,    (void**)&orig_Free },
-            {"AnoSDKRegistInfoListener",     (void*)h_Reg,     &_13},
+            {"AnoSDKInit",                   (void*)h_Init,     (void**)&orig_Init  },
+            {"AnoSDKInitEx",                 (void*)h_InitEx,   (void**)&orig_InitEx},
+            {"AnoSDKSetUserInfo",            (void*)h_SUI,      &_1 },
+            {"AnoSDKSetUserInfoWithLicense", (void*)h_SUIL,     &_2 },
+            {"AnoSDKIoctl",                  (void*)h_Ioctl,    &_3 },
+            {"AnoSDKIoctlOld",               (void*)h_IoctlOld, &_4 },
+            {"AnoSDKOnPause",                (void*)h_Pause,    &_5 },
+            {"AnoSDKOnResume",               (void*)h_Resume,   &_6 },
+            {"AnoSDKOnRecvData",             (void*)h_RecvData, &_7 },
+            {"AnoSDKOnRecvSignature",        (void*)h_RecvSig,  &_8 },
+            {"AnoSDKGetReportData",          (void*)h_GRD,      &_9 },
+            {"AnoSDKGetReportData2",         (void*)h_GRD2,     &_10},
+            {"AnoSDKGetReportData3",         (void*)h_GRD3,     &_11},
+            {"AnoSDKGetReportData4",         (void*)h_GRD4,     &_12},
+            {"AnoSDKDelReportData",          (void*)h_Del,      (void**)&orig_Del  },
+            {"AnoSDKDelReportData3",         (void*)h_Del3,     (void**)&orig_Del3 },
+            {"AnoSDKDelReportData4",         (void*)h_Del4,     (void**)&orig_Del4 },
+            {"AnoSDKFree",                   (void*)h_Free,     (void**)&orig_Free },
+            {"AnoSDKRegistInfoListener",     (void*)h_Reg,      &_13},
         };
         rebind_symbols(b, sizeof(b)/sizeof(b[0]));
     });
@@ -74,5 +74,12 @@ void AnoSDKBypassInstall(void) {
 @interface AnoSDKBypassLoader : NSObject
 @end
 @implementation AnoSDKBypassLoader
-+ (void)load { AnoSDKBypassInstall(); }
++ (void)load {
+    // +load'da değil, uygulama başladıktan sonra çalıştır
+    dispatch_after(
+        dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)),
+        dispatch_get_main_queue(),
+        ^{ AnoSDKBypassInstall(); }
+    );
+}
 @end
