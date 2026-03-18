3a0c8a16c956cb7d6fc8ef25365dc16831a42ebd
oc9201744-cmd <oc9201744@gmail.com>
2026-03-19 01:25:30 +0300

    Update pubg_offset.h

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -1,19 +1,24 @@
+//
+// Created by XBK on 2022/1/16.
+//
 #include <stdio.h>
 #include <string>
-
 namespace PubgOffset {
 
 int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
 
 namespace PlayerControllerParam {
-
-int SelfOffset = 0x28D0;          // ASTExtraPlayerController::STExtraBaseCharacter
+// ASTExtraPlayerController::STExtraBaseCharacter
+int SelfOffset = 0x28D0;
+// AController::ControlRotation
 int MouseOffset = 0x4e0;
+// APlayerController::PlayerCameraManager
 int CameraManagerOffset = 0x548;
 int AngleOffset = 0x558;
 
 namespace CameraManagerParam {
-int PovOffset = 0x520 + 0x10;     // APlayerCameraManager::CameraCache + POV
+// APlayerCameraManager::CameraCache + POV
+int PovOffset = 0x520 + 0x10;
 }
 
 namespace ControllerFunction {
@@ -22,6 +27,7 @@ int LineOfSightToOffset = 0x7B0;
 
 }
 
+// UWorld::PersistentLevel
 int ULevelOffset = 0x30;
 
 namespace ULevelParam {
@@ -40,13 +46,19 @@ int AddControllerRollInputOffset  = 0x888;
 int AddControllerPitchInputOffset = 0x898;
 }
 
-int StatusOffset  = 0x1058;       // ASTExtraCharacter::CurrentStates (uint64)
-int TeamOffset    = 0x998;        // AUAECharacter::TeamID
-int NameOffset    = 0x960;        // AUAECharacter::PlayerName (FString.data)
-int RobotOffset   = 0xa40;        // AUAECharacter::bIsAI
-int HpOffset      = 0xe60;        // ASTExtraCharacter::Health
-int HpmaxOffset   = 0xe64;        // ASTExtraCharacter::HealthMax
-int DeadOffset    = 0xe7c;        // ASTExtraCharacter::bDead
+// ASTExtraCharacter::CurrentStates (uint64)
+int StatusOffset  = 0x1058;
+// AUAECharacter::TeamID
+int TeamOffset    = 0x998;
+// AUAECharacter::PlayerName
+int NameOffset    = 0x960;
+// AUAECharacter::bIsAI
+int RobotOffset   = 0xa40;
+// ASTExtraCharacter::Health
+int HpOffset      = 0xe60;
+int HpmaxOffset   = 0xe64;
+// ASTExtraCharacter::bDead
+int DeadOffset    = 0xe7c;
 
 int VehicleCommonComponentOffset = 0xc00;
 int VehicleHPOffset              = 0x354;
@@ -55,38 +67,50 @@ int VehicleFuelOffset            = 0x43c;
 int VehicleFuelMaxOffset         = 0x438;
 
 int MoveCoordOffset = 0x110;
-int MeshOffset      = 0x510;      // ACharacter::Mesh
+// ACharacter::Mesh
+int MeshOffset      = 0x510;
 int boneCountOffset = 0x8d0;
 
 namespace MeshParam {
-int HumanOffset = 0x210;
-int BonesOffset = 0x988;          // USkinnedMeshComponent::CachedComponentSpaceTransforms
+// CachedComponentSpaceTransforms (absolute)
+int HumanOffset = 0xC40;
+// USkinnedMeshComponent::CachedBoneSpaceTransforms
+int BonesOffset = 0x988;
 }
 
-int OpenFireOffset     = 0x1800;  // ASTExtraBaseCharacter::bIsWeaponFiring
-int OpenTheSightOffset = 0x1134;  // ASTExtraCharacter::bIsGunADS
+// ASTExtraBaseCharacter::bIsWeaponFiring
+int OpenFireOffset     = 0x1800;
+// ASTExtraCharacter::bIsGunADS
+int OpenTheSightOffset = 0x1134;
 
-// Silah: character + WeaponManagerComponentOffset -> mgr + WeaponOneOffset -> weapon
+// ASTExtraBaseCharacter::WeaponManagerComponent
 int WeaponManagerComponentOffset = 0x25B8;
-int WeaponOneOffset              = 0x5C8;  // UWeaponManagerComponent::CurrentWeaponReplicated
+// UWeaponManagerComponent::CurrentWeaponReplicated
+int WeaponOneOffset              = 0x5C8;
 
 namespace WeaponParam {
-int MasterOffset    = 0x110;
-int ShootModeOffset = 0x10D9;
+int MasterOffset     = 0x110;
+// ASTExtraShootWeapon::ShootMode
+int ShootModeOffset  = 0x10D9;
+// UShootWeaponEntity* ShootWeaponEntityComp
 int WeaponAttrOffset = 0x1360;
 
 namespace WeaponAttrParam {
+// UShootWeaponEntity::BulletFireSpeed
 int BulletSpeedOffset = 0x560;
+// UShootWeaponEntity::RecoilKickADS
 int RecoilOffset      = 0xcf0;
 }
 }
 
+// APickUpListWrapperActor::PickUpDataList
 int GoodsListOffset = 0x940;
 namespace GoodsListParam {
 int DataBase = 0x38;
 }
 
-int CoordOffset = 0x208;          // AActor::RootComponent
+// AActor::RootComponent
+int CoordOffset = 0x208;
 
 namespace CoordParam {
 int HeightOffset = 0x1dc;
