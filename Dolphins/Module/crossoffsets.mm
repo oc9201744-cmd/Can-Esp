036df3ec41d5b9f0defc6eceb5fa55a1d4e532aa
oc9201744-cmd <oc9201744@gmail.com>
2026-03-20 02:15:08 +0300

    Update crossoffsets.mm

--- Dolphins/Module/crossoffsets.mm
+++ Dolphins/Module/crossoffsets.mm
@@ -3,16 +3,16 @@
 @implementation OffsetsManager
 
 + (OffsetValues)getOffsetsForBundleID:(NSString *)bundleID {
-    OffsetValues defaultOffsets =  { 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0, 0x10A34E980 };  // Yeni güncellenmiş değerler
+    OffsetValues defaultOffsets =  { 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0 };  // GL
 
 if ([bundleID containsString:@"tencent"]) {
-    return (OffsetValues){ 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0, 0x10A34E980 };
+    return (OffsetValues){ 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0 };
 } else if ([bundleID containsString:@"vng"]) {
-    return (OffsetValues){ 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0, 0x10A34E980 };
+    return (OffsetValues){ 0x1028791CC, 0x10A171A00, 0x104510EF0, 0x109AAA1A0 };
 } else if ([bundleID containsString:@"krmobile"]) {
-    return (OffsetValues){ 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0, 0x10A34E980 };
+    return (OffsetValues){ 0x102AD71F8, 0x10A47D400, 0x10476F14C, 0x109DB5940 };
 } else if ([bundleID containsString:@"rekoo"]) {
-    return (OffsetValues){ 0x102A62208, 0x10A566E00, 0x104bd8740, 0x10a1178b0, 0x10A34E980 };
+    return (OffsetValues){ 0x102AAAB0C, 0x10A453300, 0x104742830, 0x109D8B830 };
 }
 
     return defaultOffsets;
