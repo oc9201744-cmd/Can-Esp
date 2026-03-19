722dd6b838efc08cf76db46312d5d339db61aa4c
oc9201744-cmd <oc9201744@gmail.com>
2026-03-20 02:44:53 +0300

    Update pubg_offset.h

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -4,17 +4,16 @@
 namespace PubgOffset {
 
 int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
-uintptr_t gObject = 0x10A34E980;  // Global Object pointer
 
 namespace PlayerControllerParam {
 
-int SelfOffset = 0x28e0;
+int SelfOffset = 0x28d0;
 int MouseOffset = 0x4e0;
 int CameraManagerOffset = 0x548;
 int AngleOffset = 0x558;
 
 namespace CameraManagerParam {
-int PovOffset = 0x10a0 + 0x10;
+int PovOffset = 0x520 + 0x10;
 }
 
 namespace ControllerFunction {
@@ -43,17 +42,17 @@ int AddControllerPitchInputOffset = 0x898;
 
 int StatusOffset = 0x1018;
 int TeamOffset = 0x998;
-int NameOffset = 0x960;
-int RobotOffset = 0xa40;
+int NameOffset = 0x4d0;
+int RobotOffset = 0x650;
 int HpOffset = 0xe60;
 int HpmaxOffset = 0xe64;
 int DeadOffset = 0xe7c;
 
-int VehicleCommonComponentOffset = 0xc00;
-int VehicleHPOffset = 0x354;
-int VehicleHPMaxOffset = 0x350;
-int VehicleFuelOffset = 0x43c;
-int VehicleFuelMaxOffset = 0x438;
+int VehicleCommonComponentOffset = 0xbf0;
+int VehicleHPOffset = 0x344;
+int VehicleHPMaxOffset = 0x340;
+int VehicleFuelOffset = 0x424;
+int VehicleFuelMaxOffset = 0x420;
 
 int MoveCoordOffset = 0x110;
 int MeshOffset = 0x510;
@@ -64,15 +63,15 @@ int HumanOffset = 0x210;
 int BonesOffset = 0x988;
 }
 
-int OpenFireOffset = 0x1800;
-int OpenTheSightOffset = 0x10e1;
+int OpenFireOffset = 0x1788;
+int OpenTheSightOffset = 0x1134;
 
-int WeaponOneOffset = 0x2a30 + 0x20;
+int WeaponOneOffset = 0xa10;
 
 namespace WeaponParam {
 
 int MasterOffset = 0x110;
-int ShootModeOffset = 0x10d9;
+int ShootModeOffset = 0x1089;
 int WeaponAttrOffset = 0x12c0;
 
 namespace WeaponAttrParam {
@@ -82,10 +81,10 @@ int RecoilOffset = 0xcf0;
 
 }
 
-int GoodsListOffset = 0x940;
+int GoodsListOffset = 0x2EF8;
 
 namespace GoodsListParam {
-int DataBase = 0x38;
+int DataBase = 0x490;
 }
 
 int CoordOffset = 0x208;
