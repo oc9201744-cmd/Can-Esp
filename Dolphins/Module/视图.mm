583865ee421a07d3ec1bedc96d15954a2e9065bf
oc9201744-cmd <oc9201744@gmail.com>
2026-03-20 01:07:10 +0300

    Update 视图.mm

--- Dolphins/Module/视图.mm
+++ Dolphins/Module/视图.mm
@@ -170,7 +170,7 @@ id<MTLTexture> thuoTexture;
     
     imDrawList = ImGui::GetWindowDrawList();
     
-    //拉取一帧的数 - Get frame data (imDrawList obtained inside readFrameData)
+    //拉取一帧的数
     readFrameData(ImVec2(screenSize.width / 2,screenSize.height / 2),playerDataList, materialDataList);
     for (MaterialData materialData:materialDataList) {
             //判断是否在屏幕内
