3717a58adee1a41736555157e16fe3dd761905fb
oc9201744-cmd <oc9201744@gmail.com>
2026-03-22 03:57:17 +0300

    Update pubg_offset.h

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -1,54 +1,84 @@
-#pragma once
-
-namespace PubgOffset {
-    // Senin orijinal offset'ler
-    inline uintptr_t GWorldFunction = 0x10282a858;
-    inline uintptr_t GWorldData     = 0x10992a6e0;
-    inline uintptr_t GNameFunction  = 0x1044dd6ec;
-    inline uintptr_t GNameData      = 0x10953ecd0;
-
-    inline uintptr_t PlayerControllerOffset[3] = {0x30, 0x78, 0x98};
-
-    inline uintptr_t ULevelOffset          = 0x38;
-    inline uintptr_t ActorArrayOffset      = 0xA0;
-    inline uintptr_t ActorCountOffset      = 0xA8;
-
-    inline uintptr_t SelfOffset            = 0x4B8;
-    inline uintptr_t CameraManagerOffset   = 0x548;
-    inline uintptr_t LineOfSightToOffset   = 0x780;  // Vtable offset
-
-    inline uintptr_t CoordOffset           = 0x208;
-    inline uintptr_t ClassIdOffset         = 0x18;
-    inline uintptr_t HpOffset              = 0xE60;
-    inline uintptr_t DeadOffset            = 0xE7C;
-    inline uintptr_t TeamOffset            = 0x940;
-    inline uintptr_t NameOffset            = 0x960;
-    inline uintptr_t RobotOffset           = 0xA40;
-    inline uintptr_t StatusOffset          = 0x1058;
-    inline uintptr_t MeshOffset            = 0x510;
-    inline uintptr_t BonesOffset           = 0x5A0;
-
-    inline uintptr_t AddControllerYawInputOffset   = 0x780;
-    inline uintptr_t AddControllerRollInputOffset  = 0x788;
-    inline uintptr_t AddControllerPitchInputOffset = 0x790;
-
-    inline uintptr_t WeaponOneOffset       = 0x5C8;
-    inline uintptr_t OpenTheSightOffset    = 0x1134;
-    inline uintptr_t OpenFireOffset        = 0x1800;
-    inline uintptr_t ShootModeOffset       = 0x10D9;
-    inline uintptr_t WeaponAttrOffset      = 0x398;
-    inline uintptr_t BulletSpeedOffset     = 0x560;
-    inline uintptr_t RecoilOffset          = 0xCF0;
-    inline uintptr_t MoveCoordOffset       = 0x18C;
-
-    // Hata verenler için eklenenler (4.3 GL)
-    inline uintptr_t PovOffset             = 0xAA0;  // CameraManager + POV struct
-    inline uintptr_t ControlRotationOffset = 0x4D8;  // Mouse yerine yaw/pitch/roll
-
-    inline uintptr_t HeightOffset          = 0x160;  // Coord + Z height
-
-    inline uintptr_t GoodsListOffset       = 0x8F0;
-    namespace GoodsListParam {
-        inline uintptr_t DataBase          = 0x18;
-    }
-}
+#define kLineOfSightTo "0x7B0"
+#define kPersistentLevel "0x30"        
+#define kActorList "0xA0"
+#define kNetDriver "0x38"                
+#define kServerConnection "0x78"          
+#define kPlayerController "0x98"          
+#define klocalPlayerController "0x30"
+#define kPawn "0x4b8"                   
+#define kCharacter "0x4c8"            
+#define kControlRotation "0x4e0"         
+#define kMyTeam "0x940"                   
+#define kCameraCache "0x520"            
+#define kViewTarget "0x10a0"              
+#define kPlayerCameraManager "0x548"      
+#define kSizeX "0x40"
+#define kSizeY "0x44"
+#define kHealth "0xe60"                    
+#define kHealthMax "0xe64"                  
+#define kNearDeathBreath "0x1b60"
+#define kbDead "0xe7c"                      
+#define kbHidden "0xe8"                    
+#define kCurrentVehicle "0xeb0"             
+#define kCurrentStates "0x1058"
+#define kNearDeatchComponent "0x1be8"
+#define kRootComponent "0x208"    
+#define kRepMovement "0x110"               
+#define kPlayerName "0x960"                 
+#define kPlayerUID "0x988"                  
+#define kNation "0x970"                     
+#define kTeamID "0x998"                     
+#define kbIsAI "0xa40"                         
+#define kbIsMLAI "0xa41"                    
+#define kMesh "0x510"                       
+#define kStaticMesh "0x988"                 
+#define kLastRenderTime "0x490"             
+#define kVelocity "0x18c"                   
+#define kRelativeRotation "0x1f0"
+#define kRelativeLocation "0x1e4"           
+#define kRelativeScale3D "0x1fc"            
+#define kComponentVelocity "0x2c0"          
+#define kbIsWeaponFiring "0x1800"           
+#define kWeaponManagerComponent "0x25b8"    
+#define kPoseState "0x1810"                 
+#define kScopeFov "0x1c54"                  
+#define kCurrentWeaponReplicated "0x5c8"    
+#define kShootWeaponEntityComponent "0x398" 
+#define kWeaponId "0x1e0"                   
+#define kBulletFireSpeed "0x560"            
+#define kRecoilKickADS "0xcf0"              
+#define kGameDeviationFactor "0xc2c"        
+#define kShootWeaponComponent "0xf30"       
+#define kShootWeaponEntityComp "0x1360"     
+#define kShootMode "0x10d9"
+#define kbIsGunADS "0x1134"                 
+#define kVehicleCommon "0xc00"              
+#define kHP "0x354"                         
+#define kHPMax "0x350"                      
+#define kFuel "0x43c"                       
+#define kFuelMax "0x438"                    
+#define kPickUpDataList "0x940"             
+#define kGoodsID "0x38"  
+#define kTableName "0x8a0"                  
+#define kFPS "0x1c4"
+#define kSTBaseCharacter "0x28E0"
+#define kCoord "0x1dc"
+#define kHeight "0x1c8"  
+#define kBP_MapUIMarkManager_C "0x4270"     
+#define kpExtraGameState "0x338"            
+#define kAlivePlayerNum "0xb34"             
+#define kPlayerNum "0x7a8"              
+#define kelapsedSeconds "0x4a8"           
+#define kPlayerState "0x2308"             
+#define kKill "0x6c8"                       
+#define kTPP "0x1c50"                       
+#define kFPP "0x1c60"                      
+#define kGameReplayType "0x944"
+#define kPickUpAnim "0x1e28"
+#define kPressingFireBtn "0x33d0"
+#define kCurrentReloadWeapon "0x2b58"  
+#define kCachedBulletTrackComponent "0xe28"  
+#define wuhou "0x190"  
+#define kYaw "0x890"                  
+#define kRoll "0x888"                 
+#define kPitch "0x888"
