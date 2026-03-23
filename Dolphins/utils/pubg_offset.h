7e2bf92ee528685a56a20dc572695f929ca1f5b6
oc9201744-cmd <oc9201744@gmail.com>
2026-03-23 21:04:42 +0300

    Update pubg_offset.h

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -1,64 +1,100 @@
-#pragma once
+#include <stdio.h>
+#include <string>
 
 namespace PubgOffset {
 
-// ---------------- PlayerController ----------------
-static int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
+int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
 
 namespace PlayerControllerParam {
-static int SelfOffset = 0x28e0;
-static int CameraManagerOffset = 0x548;
-static int AngleOffset = 0x4e0;
+
+int SelfOffset = 0x28e0;
+int MouseOffset = 0x4e0;
+int CameraManagerOffset = 0x548;
+int AngleOffset = 0x4e0;
+
+namespace CameraManagerParam {
+int PovOffset = 0x520;
+}
 
 namespace ControllerFunction {
-static int LineOfSightToOffset = 0x7B0;
-}
+int LineOfSightToOffset = 0x7B0;
 }
 
-// ---------------- ULevel ----------------
-static int ULevelOffset = 0x30;
+}
+
+int ULevelOffset = 0x30;
 
 namespace ULevelParam {
-static int ObjectArrayOffset = 0xA0;
-static int ObjectCountOffset = 0xA8;
+int ObjectArrayOffset = 0xA0;
+int ObjectCountOffset = 0xA8;
 }
 
-// ---------------- Object ----------------
 namespace ObjectParam {
 
-// ✔ SDK’ya göre düzeltildi
-static int PlayerStateOffset = 0x4D0;
-static int bIsAI_Offset = 0x28;
+int ClassIdOffset = 0x18;
+int ClassNameOffset = 0xC;
 
-// --- Temel bilgiler
-static int TeamOffset = 0x998;
-static int NameOffset = 0x960;
+namespace PlayerFunction {
+int AddControllerYawInputOffset = 0x890;
+int AddControllerRollInputOffset = 0x888;
+int AddControllerPitchInputOffset = 0x898;
+}
 
-// --- Sağlık
-static int HpOffset = 0xe60;
-static int HpmaxOffset = 0xe64;
-static int DeadOffset = 0xe7c;
+// ===== GÜNCEL =====
+int StatusOffset = 0x1018;
+int TeamOffset = 0x9A0;   // ✅ GÜNCEL TEAM OFFSET
+int NameOffset = 0x960;
 
-// ---------------- Mesh / Bone ----------------
-static int MeshOffset = 0x510;
+// ===== FIXLER =====
+int RobotOffset = 0xa40;
+int HpOffset = 0xe60;
+int HpmaxOffset = 0xe64;
+int DeadOffset = 0xe7c;
+
+int VehicleCommonComponentOffset = 0xbf0;
+int VehicleHPOffset = 0x344;
+int VehicleHPMaxOffset = 0x340;
+int VehicleFuelOffset = 0x424;
+int VehicleFuelMaxOffset = 0x420;
+
+int MoveCoordOffset = 0x110;
+int MeshOffset = 0x510;
+int boneCountOffset = 0x8d0;
 
 namespace MeshParam {
-static int BonePtrOffset = 0x990;
-static int BoneBaseOffset = 0x208;
+int HumanOffset = 0x210;
+int BonesOffset = 0x990;
 }
 
-// ---------------- Koordinat ----------------
-static int RootComponentOffset = 0x208;
+int OpenFireOffset = 0x1800;
+int OpenTheSightOffset = 0x1134;
+
+int WeaponOneOffset = 0x2a30 + 0x20;
+
+namespace WeaponParam {
+
+int MasterOffset = 0x110;
+int ShootModeOffset = 0x1089;
+int WeaponAttrOffset = 0x12c0;
+
+namespace WeaponAttrParam {
+int BulletSpeedOffset = 0x560;
+int RecoilOffset = 0xcf0;
+}
+
+}
+
+int GoodsListOffset = 0x940;
+
+namespace GoodsListParam {
+int DataBase = 0x38;
+}
+
+int CoordOffset = 0x208;
 
 namespace CoordParam {
-static int RelativeLocation = 0x1e4;
-}
-
-// ---------------- Player Input ----------------
-namespace PlayerFunction {
-static int AddControllerYawInputOffset = 0x890;
-static int AddControllerRollInputOffset = 0x888;
-static int AddControllerPitchInputOffset = 0x898;
+int HeightOffset = 0x1e4;
+int CoordOffset = 0x1e4;
 }
 
 }
