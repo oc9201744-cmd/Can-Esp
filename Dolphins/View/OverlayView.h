419f001614b966e05aa8b854074679ffc5c1e67d
oc9201744-cmd <oc9201744@gmail.com>
2026-03-25 22:35:34 +0300

    Update OverlayView.h

--- Dolphins/View/OverlayView.h
+++ Dolphins/View/OverlayView.h
@@ -1,31 +1,14 @@
-#import "Dolphins/View/CustomView/UIView+YYAdd.h"
+//
+//  OverlayView.h
+//  Dolphins
+//
 
+#import <UIKit/UIKit.h>
+#import "Dolphins/utils/module_tools.h"
 
-#import <Metal/Metal.h>
-#import <MetalKit/MetalKit.h>
-#include "Dolphins/Module/视图.h"
-#include "Dolphins/Module/菜单.h"
+@class mao;
+@class mi;
 
-
-#include "Dolphins/imgui/imgui_impl_metal.h"
-
-#include "Dolphins/utils/module_tools.h"
-
-#include "Dolphins/utils/imgui_tools.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface OverlayView : MTKView <MTKViewDelegate>
-@property (nonatomic, strong) id <MTLCommandQueue> commandQueue;
-@property (nonatomic, strong) MTKTextureLoader *loader;
-
-@property (nonatomic, assign) ModuleControl *moduleControl;
-
-@property (nonatomic, strong) mao *mao;
-@property (nonatomic, strong) mi *mi;
-
-- (instancetype)initWithFrame:(CGRect)frame :(ModuleControl*)control :(mao*)draw :(mi*)menu;
-
+@interface OverlayView : UIView
+- (instancetype)initWithFrame:(CGRect)frame :(ModuleControl*)control :(mao*)drawWindow :(mi*)menuWindow;
 @end
-
-NS_ASSUME_NONNULL_END
