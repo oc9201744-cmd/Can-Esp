292b507731b2227b1fb92caaaa0711ecd377df79
oc9201744-cmd <oc9201744@gmail.com>
2026-03-25 21:23:09 +0300

    Update PUBG offsets in pubg_offset.h

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -3,40 +3,30 @@
 
 namespace PubgOffset {
 
-// Global offsets from your header - Slide 0
-namespace Global {
-    const long gobject = 0x10A34E980;
-    const long gname_func = 0x104bd8740;
-    const long gname_data = 0x10a1178b0;
-    const long gworld_func = 0x102A62208;
-    const long gworld_data = 0x10A566E00;
-}
-
-// PlayerController chain: NetDriver -> ServerConnection -> PlayerController
 int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
 
 namespace PlayerControllerParam {
 
-int SelfOffset = 0x28E0;          // kSTBaseCharacter
-int MouseOffset = 0x4e0;          // kControlRotation
-int CameraManagerOffset = 0x548;  // kPlayerCameraManager
+int SelfOffset = 0x28D0;          // ASTExtraPlayerController::STExtraBaseCharacter
+int MouseOffset = 0x4e0;
+int CameraManagerOffset = 0x548;
 int AngleOffset = 0x558;
 
 namespace CameraManagerParam {
-int PovOffset = 0x520 + 0x10;     // kCameraCache + 0x10 = 0x530
+int PovOffset = 0x520 + 0x10;     // APlayerCameraManager::CameraCache + POV
 }
 
 namespace ControllerFunction {
-int LineOfSightToOffset = 0x7B0;  // kLineOfSightTo
+int LineOfSightToOffset = 0x7B0;
 }
 
 }
 
-int ULevelOffset = 0x30;            // kPersistentLevel
+int ULevelOffset = 0x30;
 
 namespace ULevelParam {
-int ObjectArrayOffset = 0xA0;       // kActorList
-int ObjectCountOffset = 0xA8;       // kActorList + 0x8
+int ObjectArrayOffset = 0xA0;
+int ObjectCountOffset = 0xA8;
 }
 
 namespace ObjectParam {
@@ -45,62 +35,62 @@ int ClassIdOffset = 0x18;
 int ClassNameOffset = 0xC;
 
 namespace PlayerFunction {
-int AddControllerYawInputOffset = 0x890;   // kYaw
-int AddControllerRollInputOffset = 0x888;  // kRoll
-int AddControllerPitchInputOffset = 0x898; // kPitch
+int AddControllerYawInputOffset   = 0x890;
+int AddControllerRollInputOffset  = 0x888;
+int AddControllerPitchInputOffset = 0x898;
 }
 
-int StatusOffset = 0x1058;        // kCurrentStates
-int TeamOffset = 0x998;           // kTeamID
-int NameOffset = 0x960;           // kPlayerName
-int RobotOffset = 0xa40;          // kbIsAI
-int HpOffset = 0xe60;             // kHealth
-int HpmaxOffset = 0xe64;          // kHealthMax
-int DeadOffset = 0xe7c;           // kbDead
+int StatusOffset  = 0x1058;       // ASTExtraCharacter::CurrentStates (uint64)
+int TeamOffset    = 0x998;        // AUAECharacter::TeamID
+int NameOffset    = 0x960;        // AUAECharacter::PlayerName (FString.data)
+int RobotOffset   = 0xa40;        // AUAECharacter::bIsAI
+int HpOffset      = 0xe60;        // ASTExtraCharacter::Health
+int HpmaxOffset   = 0xe64;        // ASTExtraCharacter::HealthMax
+int DeadOffset    = 0xe7c;        // ASTExtraCharacter::bDead
 
-int VehicleCommonComponentOffset = 0xc00;  // kVehicleCommon
-int VehicleHPOffset = 0x354;      // kHP
-int VehicleHPMaxOffset = 0x350;   // kHPMax
-int VehicleFuelOffset = 0x43c;    // kFuel
-int VehicleFuelMaxOffset = 0x438; // kFuelMax
+int VehicleCommonComponentOffset = 0xc00;
+int VehicleHPOffset              = 0x354;
+int VehicleHPMaxOffset           = 0x350;
+int VehicleFuelOffset            = 0x43c;
+int VehicleFuelMaxOffset         = 0x438;
 
-int MoveCoordOffset = 0x110;      // kRepMovement
-int MeshOffset = 0x510;           // kMesh
+int MoveCoordOffset = 0x110;
+int MeshOffset      = 0x510;      // ACharacter::Mesh
 int boneCountOffset = 0x8d0;
 
 namespace MeshParam {
-int HumanOffset = 0x210;          // kRelativeLocation base
-int BonesOffset = 0x988;          // kStaticMesh
+int HumanOffset = 0x210;
+int BonesOffset = 0x988;          // USkinnedMeshComponent::CachedComponentSpaceTransforms
 }
 
-int OpenFireOffset = 0x1800;      // kbIsWeaponFiring
-int OpenTheSightOffset = 0x1134;  // kbIsGunADS
+int OpenFireOffset     = 0x1800;  // ASTExtraBaseCharacter::bIsWeaponFiring
+int OpenTheSightOffset = 0x1134;  // ASTExtraCharacter::bIsGunADS
 
-int WeaponManagerComponentOffset = 0x25b8;  // kWeaponManagerComponent
-int WeaponOneOffset = 0x5c8;      // kCurrentWeaponReplicated
+// Silah: character + WeaponManagerComponentOffset -> mgr + WeaponOneOffset -> weapon
+int WeaponManagerComponentOffset = 0x25B8;
+int WeaponOneOffset              = 0x5C8;  // UWeaponManagerComponent::CurrentWeaponReplicated
 
 namespace WeaponParam {
-int MasterOffset = 0x110;
-int ShootModeOffset = 0x10d9;    // kShootMode
-int WeaponAttrOffset = 0x398;     // kShootWeaponEntityComponent
+int MasterOffset    = 0x110;
+int ShootModeOffset = 0x10D9;
+int WeaponAttrOffset = 0x1360;
 
 namespace WeaponAttrParam {
-int BulletSpeedOffset = 0x560;    // kBulletFireSpeed
-int RecoilOffset = 0xcf0;         // kRecoilKickADS
+int BulletSpeedOffset = 0x560;
+int RecoilOffset      = 0xcf0;
 }
 }
 
-int GoodsListOffset = 0x940;      // kPickUpDataList
-
+int GoodsListOffset = 0x940;
 namespace GoodsListParam {
-int DataBase = 0x38;              // kGoodsID
+int DataBase = 0x38;
 }
 
-int CoordOffset = 0x208;          // kRootComponent
+int CoordOffset = 0x208;          // AActor::RootComponent
 
 namespace CoordParam {
-int HeightOffset = 0x1dc;         // kCoord
-int CoordOffset = 0x1e4;          // kRelativeLocation
+int HeightOffset = 0x1dc;
+int CoordOffset  = 0x1c8;
 }
 
 }
