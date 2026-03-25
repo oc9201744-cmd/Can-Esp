72352ae83342a9b249ee545ced37c99057cc5a0e
oc9201744-cmd <oc9201744@gmail.com>
2026-03-25 22:47:53 +0300

    Refactor OverlayView to use MetalKit
    Updated OverlayView to inherit from MTKView and added Metal support.

--- Dolphins/View/OverlayView.h
+++ Dolphins/View/OverlayView.h
@@ -1,14 +1,31 @@
-//
-//  OverlayView.h
-//  Dolphins
-//
+#import "Dolphins/View/CustomView/UIView+YYAdd.h"
 
-#import <UIKit/UIKit.h>
-#import "Dolphins/utils/module_tools.h"
 
-@class mao;
-@class mi;
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#include "Dolphins/Module/视图.h"
+#include "Dolphins/Module/菜单.h"
 
-@interface OverlayView : UIView
-- (instancetype)initWithFrame:(CGRect)frame :(ModuleControl*)control :(mao*)drawWindow :(mi*)menuWindow;
+
+#include "Dolphins/imgui/imgui_impl_metal.h"
+
+#include "Dolphins/utils/module_tools.h"
+
+#include "Dolphins/utils/imgui_tools.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OverlayView : MTKView <MTKViewDelegate>
+@property (nonatomic, strong) id <MTLCommandQueue> commandQueue;
+@property (nonatomic, strong) MTKTextureLoader *loader;
+
+@property (nonatomic, assign) ModuleControl *moduleControl;
+
+@property (nonatomic, strong) mao *mao;
+@property (nonatomic, strong) mi *mi;
+
+- (instancetype)initWithFrame:(CGRect)frame :(ModuleControl*)control :(mao*)draw :(mi*)menu;
+
 @end
+
+NS_ASSUME_NONNULL_END
