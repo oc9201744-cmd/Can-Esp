0b221be5839534bad5406f10af68d150e6d938bf
oc9201744-cmd <oc9201744@gmail.com>
2026-03-25 22:35:01 +0300

    Update FloatView.h

--- Dolphins/View/FloatView.h
+++ Dolphins/View/FloatView.h
@@ -1,20 +1,11 @@
-#import "Dolphins/View/CustomView/UIView+YYAdd.h"
-#include "Dolphins/utils/module_tools.h"
-NS_ASSUME_NONNULL_BEGIN
+//
+//  FloatView.h
+//  Dolphins
+//
+
+#import <UIKit/UIKit.h>
+#import "Dolphins/utils/module_tools.h"
 
 @interface FloatView : UIView
-
-@property (nonatomic, strong) UIImageView *iconImageView;
-
-@property (nonatomic, assign) CGPoint startLocation;
-@property (nonatomic, assign) CGPoint didMoveLocation;
-
-@property (nonatomic, assign) ModuleControl *moduleControl;
-
 - (instancetype)initWithFrame:(CGRect)frame :(ModuleControl*)control;
-
-- (void)iconOnClick;
-
 @end
-
-NS_ASSUME_NONNULL_END
