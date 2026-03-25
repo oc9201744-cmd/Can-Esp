24fbae4c8c932d223b99aa48c41df756e437a58a
oc9201744-cmd <oc9201744@gmail.com>
2026-03-25 22:33:01 +0300

    Update dolphins.h

--- Dolphins/dolphins.h
+++ Dolphins/dolphins.h
@@ -5,12 +5,18 @@
 
 #pragma once
 
-// Önce C++ standard library
 #include <vector>
 #include <string>
 #include <cmath>
 
-// ImVec2, ImVec3 tanımları
+// imgui.h ile çakışmayı önlemek için
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_DEFINE_MATH_OPERATORS
+#endif
+
+// Sadece imgui yoksa tanımla
+#ifndef IMGUI_H
+
 struct ImVec2 {
     float x, y;
     ImVec2() : x(0), y(0) {}
@@ -25,7 +31,10 @@ struct ImVec3 {
 
 struct ImVec4 {
     float x, y, z, w;
+    ImVec4() : x(0), y(0), z(0), w(0) {}
 };
+
+#endif
 
 struct FRotator {
     float Pitch, Yaw, Roll;
@@ -35,6 +44,16 @@ struct MinimalViewInfo {
     ImVec3 location;
     FRotator rotation;
     float fov;
+};
+
+struct Ue4Transform {
+    ImVec4 rotation;
+    ImVec3 translation;
+    ImVec3 scale3d;
+};
+
+struct Ue4Matrix {
+    float m[4][4];
 };
 
 struct BonesData {
@@ -94,7 +113,7 @@ struct MaterialStruct {
     char* name = nullptr;
 };
 
-// Forward declarations - sadece bildirim, tanım .mm dosyasında
+// Function declarations
 float get3dDistance(ImVec3 a, ImVec3 b, float scale);
 float get2dDistance(ImVec2 screen, ImVec2 point);
 float rotateAngle(ImVec3 from, ImVec3 to);
@@ -103,10 +122,19 @@ ImVec2 worldToScreen(ImVec3 worldPos, MinimalViewInfo pov, ImVec2 screenSize);
 float getAngleDifference(float target, float current);
 float change(float angle);
 ImVec2 rotateAngleView(ImVec3 from, ImVec3 to);
+Ue4Matrix transformToMatrix(Ue4Transform transform);
+ImVec3 matrixToVector(Ue4Matrix matrix);
+Ue4Matrix matrixMulti(Ue4Matrix a, Ue4Matrix b);
 MaterialStruct isMaterial(const char* className);
 MaterialStruct isWeapon(const char* className);
 MaterialStruct isBoxMaterial(int itemId);
 bool isRecycled(const char* className);
-std::string getStatusName(uintptr_t statusAddr);
-void readFrameData(ImVec2 screenSize, std::vector<PlayerData> &playerDataList, std::vector<MaterialData> &materialDataList);
+char* getClassName(int classId);
+char* getPlayerName(uintptr_t addr);
+ImVec3 getBone(uintptr_t human, uintptr_t bones, int part);
 bool getBone2d(MinimalViewInfo pov, ImVec2 screen, uintptr_t human, uintptr_t bones, int part, ImVec2 &buf);
+bool isCoordVisibility(ImVec3 coord);
+bool isOnSmoke(ImVec3 coord);
+void* readStaticData(void*);
+void* silenceAimbot(void*);
+void readFrameData(ImVec2 screenSize, std::vector<PlayerData>& playerDataList, std::vector<MaterialData>& materialDataList);
