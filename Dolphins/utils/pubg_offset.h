52e2e1df44821978563f3d8994126756aa884c92
oc9201744-cmd <oc9201744@gmail.com>
2026-03-25 20:49:46 +0300

    Refactor PUBG offsets into structured namespaces
    Refactored PUBG offsets into namespaces for better organization and added new offsets for player and vehicle parameters.

--- Dolphins/utils/pubg_offset.h
+++ Dolphins/utils/pubg_offset.h
@@ -3,106 +3,100 @@
 
 namespace PubgOffset {
 
-int PlayerControllerOffset[3] = {0x38, 0x78, 0x30};
+namespace Global {
+    const long gobject     = 0x10A34E980;
+    const long gname_func  = 0x104BD8740;
+    const long gname_data  = 0x10A1178B0;
+    const long gworld_func = 0x102A62208;
+    const long gworld_data = 0x10A566E00;
+}
+
+int PlayerControllerOffset[4] = {0x38, 0x78, 0x98, 0x30};
 
 namespace PlayerControllerParam {
+    int SelfOffset          = 0x28E0;
+    int MouseOffset         = 0x4E0;
+    int CameraManagerOffset = 0x548;
+    int AngleOffset         = 0x558;
 
-int SelfOffset = 0x28e0;
-int MouseOffset = 0x4e0;
-int CameraManagerOffset = 0x548;
-int AngleOffset = 0x558;
+    namespace CameraManagerParam {
+        int PovOffset = 0x520 + 0x10;
+    }
 
-namespace CameraManagerParam {
-int PovOffset = 0x530;  // AIOHeader: APlayerCameraManager::CameraCache.POV (0x520 + 0x10)
-}
-
-namespace ControllerFunction {
-int LineOfSightToOffset = 0x7B0;
-}
-
+    namespace ControllerFunction {
+        int LineOfSightToOffset = 0x7B0;
+    }
 }
 
 int ULevelOffset = 0x30;
 
 namespace ULevelParam {
-int ObjectArrayOffset = 0xA0;
-int ObjectCountOffset = 0xA8;
+    int ObjectArrayOffset = 0xA0;
+    int ObjectCountOffset = 0xA8;
 }
 
 namespace ObjectParam {
+    int ClassIdOffset   = 0x18;
+    int ClassNameOffset = 0xC;
 
-int ClassIdOffset = 0x18;
-int ClassNameOffset = 0xC;
+    namespace PlayerFunction {
+        int AddControllerYawInputOffset   = 0x890;
+        int AddControllerRollInputOffset  = 0x888;
+        int AddControllerPitchInputOffset = 0x888;
+    }
 
-namespace PlayerFunction {
-int AddControllerYawInputOffset = 0x890;
-int AddControllerRollInputOffset = 0x888;
-int AddControllerPitchInputOffset = 0x898;
-}
+    int StatusOffset  = 0x1058;
+    int TeamOffset    = 0x998;
+    int NameOffset    = 0x960;
+    int RobotOffset   = 0xA40;
+    int HpOffset      = 0xE60;
+    int HpmaxOffset   = 0xE64;
+    int DeadOffset    = 0xE7C;
 
-// ============ GÜNCELLENEN OFFSET'LER ============
-int StatusOffset = 0x1058;      // Özel - STExtraPlayerCharacter::CurrentStates
-int TeamOffset = 0x998;         // APlayerState::TeamID (doğru)
-int NameOffset = 0x4B8;         // APlayerState::PlayerName (AIOHeader'dan - 0x960 değil!)
-int RobotOffset = 0x4DC;        // APlayerState::bIsABot bit 2 (AIOHeader'dan - 0xa49 değil!)
-int HpOffset = 0xE60;           // STExtraPlayerCharacter::Health (AIOHeader'dan)
-int HpmaxOffset = 0xE64;        // STExtraPlayerCharacter::HealthMax
-int DeadOffset = 0xE7C;         // STExtraPlayerCharacter::bDead
+    int VehicleCommonComponentOffset = 0xC00;
+    int VehicleHPOffset     = 0x354;
+    int VehicleHPMaxOffset  = 0x350;
+    int VehicleFuelOffset   = 0x43C;
+    int VehicleFuelMaxOffset = 0x438;
 
-// Araç offset'leri
-int VehicleCommonComponentOffset = 0xC00;   // STExtraPlayerCharacter::VehicleCommonComponent
-int VehicleHPOffset = 0x354;                // STExtraVehicle::HP
-int VehicleHPMaxOffset = 0x350;             // STExtraVehicle::HPMax
-int VehicleFuelOffset = 0x43C;              // STExtraVehicle::Fuel
-int VehicleFuelMaxOffset = 0x438;           // STExtraVehicle::FuelMax
+    int MoveCoordOffset = 0x110;
+    int MeshOffset      = 0x510;
+    int boneCountOffset = 0x8D0;
 
-int MoveCoordOffset = 0x110;
-int MeshOffset = 0x510;                     // ACharacter::Mesh
-int boneCountOffset = 0x8d0;
+    namespace MeshParam {
+        int HumanOffset = 0x210;
+        int BonesOffset = 0x988;
+    }
 
-namespace MeshParam {
-int HumanOffset = 0x1E4;                    // USceneComponent::RelativeLocation (AIOHeader'dan - 0x210 değil!)
-int BonesOffset = 0xC40;                    // USkeletalMeshComponent::CachedComponentSpaceTransforms (AIOHeader'dan - 0x988 değil!)
-}
+    int OpenFireOffset     = 0x1800;
+    int OpenTheSightOffset = 0x1134;
 
-int OpenFireOffset = 0x1800;                // STExtraWeapon::bIsFiring
-int OpenTheSightOffset = 0x1134;            // STExtraWeapon::bIsADS
+    int WeaponManagerComponentOffset = 0x25B8;
+    int WeaponOneOffset = 0x5C8;
 
-int WeaponOneOffset = 0x5C8;                // STExtraWeaponManagerComponent::CurrentWeaponReplicated
+    namespace WeaponParam {
+        int MasterOffset     = 0x110;
+        int ShootModeOffset  = 0x10D9;
+        int WeaponAttrOffset = 0x398;
 
-namespace WeaponParam {
+        namespace WeaponAttrParam {
+            int BulletSpeedOffset = 0x560;
+            int RecoilOffset      = 0xCF0;
+        }
+    }
 
-int MasterOffset = 0x110;
-int ShootModeOffset = 0x10D9;               // STExtraWeapon::ShootMode
-int WeaponAttrOffset = 0x398;               // STExtraWeapon::ShootWeaponEntityComponent
+    int GoodsListOffset = 0x940;
 
-namespace WeaponAttrParam {
-int BulletSpeedOffset = 0x560;              // USTExtraShootWeaponEntityComponent::BulletFireSpeed
-int RecoilOffset = 0xCF0;                   // USTExtraShootWeaponEntityComponent::RecoilKickADS
-}
+    namespace GoodsListParam {
+        int DataBase = 0x38;
+    }
 
-}
+    int CoordOffset = 0x208;
 
-int GoodsListOffset = 0x940;                // STExtraPlayerCharacter::PickUpDataList
-
-namespace GoodsListParam {
-int DataBase = 0x38;                        // FItemDefineID::ID
-}
-
-int CoordOffset = 0x208;                    // AActor::RootComponent
-
-namespace CoordParam {
-int HeightOffset = 0x1E4;                   // USceneComponent::RelativeLocation.Z
-int CoordOffset = 0x1E4;                    // USceneComponent::RelativeLocation
-}
-
-}
-
-// ============ YARDIMCI FONKSİYON (Bot kontrolü için) ============
-inline bool IsPlayerBot(uintptr_t PlayerState) {
-    if (!PlayerState) return false;
-    uint8_t flags = *(uint8_t*)(PlayerState + ObjectParam::RobotOffset);
-    return (flags & 0x4) != 0;  // bIsABot bit 2'de
+    namespace CoordParam {
+        int HeightOffset = 0x1C8;
+        int CoordOffset  = 0x1E4;
+    }
 }
 
 }
