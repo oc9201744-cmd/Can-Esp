2bdae14d951127e60ed24de8c1e69ac55b165405
oc9201744-cmd <oc9201744@gmail.com>
2026-03-25 23:31:35 +0300

    Delete Ofset.hpp

--- Ofset.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-
-namespace UEPointers {
-    constexpr uintptr_t Names = 0x10d4a8ab8;
-    constexpr uintptr_t UObjectArray = 0x10a34e980;
-    constexpr uintptr_t Engine = 0x10a565bf0;
-    constexpr uintptr_t ProcessEvent = 0x104d7bfc0;
-}
