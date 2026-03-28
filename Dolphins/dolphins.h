5d12d40e370e37da69ca9dd350254287e9a71b82
oc9201744-cmd <oc9201744@gmail.com>
2026-03-28 20:59:25 +0300

    Update dolphins.h

--- Dolphins/dolphins.h
+++ Dolphins/dolphins.h
@@ -1,15 +1,20 @@
 //
-// dolphins.h - FIXED VERSION
-// Function declarations ONLY - NO struct definitions
+// dolphins.h - MINIMAL VERSION
+// Forward declarations only - includes handled elsewhere
 //
 
 #ifndef DOLPHINS_H
 #define DOLPHINS_H
 
 #include <vector>
-#include "CustomStrcut.h"  // FIXED - Dolphins/ prefix removed
+#include "Dolphins/imgui/imgui.h"
 
 using namespace std;
+
+// Forward declarations for structs (defined in CustomStrcut.h)
+struct PlayerData;
+struct MaterialData;
+struct MinimalViewInfo;
 
 // ============================================================================
 // FUNCTION DECLARATIONS
@@ -28,7 +33,7 @@ bool getBone2d(MinimalViewInfo pov, ImVec2 screen, uintptr_t human, uintptr_t bo
 bool isCoordVisibility(ImVec3 coord);
 bool isOnSmoke(ImVec3 coord);
 
-// NOTE: All struct definitions are in CustomStrcut.h
-// DO NOT define structs here to avoid redefinition errors!
+// NOTE: Full struct definitions are in CustomStrcut.h
+// This file uses forward declarations to avoid include issues
 
 #endif // DOLPHINS_H
