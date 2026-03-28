5125a2b9a2d326f1094cc4e1bbd2c8bf2f71f5e3
oc9201744-cmd <oc9201744@gmail.com>
2026-03-28 21:37:30 +0300

    Update dolphins.h

--- Dolphins/dolphins.h
+++ Dolphins/dolphins.h
@@ -1,39 +1,34 @@
 //
-// dolphins.h - MINIMAL VERSION
-// Forward declarations only - includes handled elsewhere
+//  Dolphins.h
+//  Dolphins
 //
-
-#ifndef DOLPHINS_H
-#define DOLPHINS_H
-
-#include <vector>
+//  Created by XBK on 2022/4/24.
+//
 #include "Dolphins/imgui/imgui.h"
 
-using namespace std;
 
-// Forward declarations for structs (defined in CustomStrcut.h)
-struct PlayerData;
-struct MaterialData;
-struct MinimalViewInfo;
-
-// ============================================================================
-// FUNCTION DECLARATIONS
-// ============================================================================
-
-// Main data reading functions
+//静态数据线程
 void *readStaticData(void *);
-void *silenceAimbot(void *);
-void readFrameData(ImVec2 screenSize, vector<PlayerData> &playerDataList, vector<MaterialData> &materialDataList);
 
-// Helper functions
-char *getPlayerName(uintptr_t addr);
-char *getClassName(int classId);
-ImVec3 getBone(uintptr_t human, uintptr_t bones, int part);
-bool getBone2d(MinimalViewInfo pov, ImVec2 screen, uintptr_t human, uintptr_t bones, int part, ImVec2 &buf);
+//获取帧数据
+void readFrameData(ImVec2 screenSize,std::vector<PlayerData> &playerDataList, std::vector<MaterialData> &materialDataList);
+
+//自瞄
+void *silenceAimbot(void *);
+    
+//掩体判断
 bool isCoordVisibility(ImVec3 coord);
+//是否在烟雾内
 bool isOnSmoke(ImVec3 coord);
 
-// NOTE: Full struct definitions are in CustomStrcut.h
-// This file uses forward declarations to avoid include issues
+//取人物名字
+char* getPlayerName(uintptr_t addr);
 
-#endif // DOLPHINS_H
+//取对象类型名
+char* getClassName(int classId);
+char* statusName(int statusId);
+//取骨骼点坐标
+ImVec3 getBone(uintptr_t human, uintptr_t bones, int part);
+
+//取骨骼点坐标(屏幕)
+bool getBone2d(MinimalViewInfo pov,ImVec2 screen, uintptr_t human, uintptr_t bones, int part,ImVec2 &buf);
